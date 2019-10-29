8f5d7aaedb0f110c74cea55bc886705718b0d1ec
NastuzziSamy <samy@nastuzzi.fr>
2019-10-29 22:44:49 +0100

    Add validations used by fields

---
+++ src/Validations/ValidationManager.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Define the validation manager class.
+ *
+ * @author Samy Nastuzzi <samy@nastuzzi.fr>
+ *
+ * @copyright Copyright (c) 2019
+ * @license MIT
+ */
+
+namespace Laramore\Validations;
+
+use Illuminate\Database\Eloquent\Model;
+use Laramore\Observers\BaseManager;
+
+class ValidationManager extends BaseManager
+{
+    /**
+     * Allowed observable sub class.
+     *
+     * @var string
+     */
+    protected $managedClass = Model::class;
+
+    /**
+     * The observable handler class to generate.
+     *
+     * @var string
+     */
+    protected $handlerClass = ValidationHandler::class;
+}
