17a9f8b4ae799e36d8a6638727e830b5689782fa
NastuzziSamy <samy@nastuzzi.fr>
2019-11-21 21:44:09 +0100

    Split constraints and fields providers

--- src/Providers/FieldsProvider.php
+++ src/Providers/FieldsProvider.php
@@ -11,21 +11,11 @@
 namespace Laramore\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Laramore\Interfaces\{
-	IsALaramoreManager, IsALaramoreProvider
-};
 use Laramore\Traits\Provider\MergesConfig;
 
-class FieldsProvider extends ServiceProvider implements IsALaramoreProvider
+class FieldsProvider extends ServiceProvider
 {
     use MergesConfig;
-
-    /**
-     * Constraint manager.
-     *
-     * @var array
-     */
-    protected static $managers;
 
     /**
      * Before booting, create our definition for migrations.
@@ -37,12 +27,6 @@ class FieldsProvider extends ServiceProvider implements IsALaramoreProvider
         $this->mergeConfigFrom(
             __DIR__.'/../../config/fields.php', 'fields',
         );
-
-        $this->app->singleton('Constraints', function() {
-            return static::getManager();
-        });
-
-        $this->app->booted([$this, 'bootedCallback']);
     }
 
     /**
@@ -55,54 +39,5 @@ class FieldsProvider extends ServiceProvider implements IsALaramoreProvider
         $this->publishes([
             __DIR__.'/../../config/fields.php' => config_path('fields.php'),
         ]);
-    }
-
-    /**
-     * Return the default values for the manager of this provider.
-     *
-     * @return array
-     */
-    public static function getDefaults(): array
-    {
-        return [];
-    }
-
-    /**
-     * Generate the corresponded manager.
-     *
-     * @param  string $key
-     * @return IsALaramoreManager
-     */
-    public static function generateManager(string $key): IsALaramoreManager
-    {
-        $class = config('fields.constraints.manager');
-
-        return static::$managers[$key] = new $class();
-    }
-
-    /**
-     * Return the generated manager for this provider.
-     *
-     * @return IsALaramoreManager
-     */
-    public static function getManager(): IsALaramoreManager
-    {
-        $appHash = \spl_object_hash(app());
-
-        if (!isset(static::$managers[$appHash])) {
-            return static::generateManager($appHash);
-        }
-
-        return static::$managers[$appHash];
-    }
-
-    /**
-     * Lock all managers after booting.
-     *
-     * @return void
-     */
-    public function bootedCallback()
-    {
-        static::getManager()->lock();
     }
 }
