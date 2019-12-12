a28870513be5954759ed69e6e31b15629170c49a
NastuzziSamy <samy@nastuzzi.fr>
2019-12-12 01:00:13 +0100

    Add incrementing field trait

--- src/Fields/Increment.php
+++ src/Fields/Increment.php
@@ -10,10 +10,12 @@
 
 namespace Laramore\Fields;
 
-use Laramore\Interfaces\IsALaramoreModel;
+use Laramore\Interfaces\{
+    IsALaramoreModel, IsAnIncrementingField
+};
 use Laramore\Elements\Type;
 
-class Increment extends Integer
+class Increment extends Integer implements IsAnIncrementingField
 {
     /**
      * Return the type object of the field.
@@ -25,23 +27,12 @@ class Increment extends Integer
         return $this->resolveType();
     }
 
-    public function getPropertyKeys(): array
-    {
-        $keys = parent::getPropertyKeys();
-
-        if (!\is_null($index = \array_search('unsigned', $keys))) {
-            unset($keys[$index]);
-        }
-
-        return $keys;
-    }
-
-    public function increment(IsALaramoreModel $model, int $value, int $increment=1)
+    public function increment(IsALaramoreModel $model, $value, $increment=1)
     {
         return $model->setAttribute($this->attname, ($value + $increment));
     }
 
-    public function decrement(IsALaramoreModel $model, int $value, int $decrement=1)
+    public function decrement(IsALaramoreModel $model, $value, $decrement=1)
     {
         return $this->increment($model, $value, - $decrement);
     }
